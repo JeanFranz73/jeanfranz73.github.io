c1d1f0c7758722758b31ad9202eec17a7a1c253b
JeanFranz73 <franzjean37@gmail.com>
2021-01-29 14:38:59 -0300

    Edit

--- tinyurl/script.js
+++ tinyurl/script.js
@@ -1,37 +1,35 @@
 function link() {
 
-    document.getElementById("result").value = "Loading...";
-    document.getElementById("result").classList.remove("is-invalid");
-    document.getElementById("btn-copy").setAttribute("disabled", true);
+    document.getElementById("result").value = "Loading..."
+    document.getElementById("result").classList.remove("is-invalid")
+    document.getElementById("btn-copy").setAttribute("disabled", "true")
 
-    var url = document.getElementById("link").value;
-    let request = new XMLHttpRequest();
+    let url = document.getElementById("link").value
+    let request = new XMLHttpRequest()
 
-    request.open('GET', `https://tinyurl.com/api-create.php?url=${url}`, true);
-    request.send();
+    request.open('GET', `https://tinyurl.com/api-create.php?url=${url}`, true)
+    request.send()
 
     request.onreadystatechange =  function () {
-        if (request.status == 200) {
-            document.getElementById("result").value = request.response;
+        if (request.status === 200) {
+            document.getElementById("result").value = request.response
             
-            document.getElementById("result").classList.remove("is-invalid");
-            document.getElementById("result").removeAttribute("disabled");
-            document.getElementById("btn-copy").removeAttribute("disabled");
+            document.getElementById("result").classList.remove("is-invalid")
+            document.getElementById("result").removeAttribute("disabled")
+            document.getElementById("btn-copy").removeAttribute("disabled")
 
         } else {
-            document.getElementById("result").value = "Unexpected error, try again";
-
-            document.getElementById("result").classList.add("is-invalid");
-            document.getElementById("result").setAttribute("disabled", true);
-            document.getElementById("btn-copy").setAttribute("disabled", true);
+            document.getElementById("result").classList.add("is-invalid")
+            document.getElementById("result").setAttribute("disabled", "true")
+            document.getElementById("btn-copy").setAttribute("disabled", "true")
+            document.getElementById("result").value = "Unexpected error, try again"
         }
     }
 }
 
 function copy() {
-    var copyText = document.getElementById("result");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    document.execCommand("copy");
-
+    let copyText = document.getElementById("result")
+    copyText.select()
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy")
 }
